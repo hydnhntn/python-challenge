901bd44e6c166828d52aad268c24a03ce4d60668
hydnhntn <hhhbhhh@gmail.com>
2022-01-09 21:10:20 -0500

    PyBank main.py prep

---
+++ PyBank/main.py
@@ -0,0 +1,15 @@
+# PyBank 
+# Calculate the following:
+# The total number of months included in the dataset
+# The net total amount of "Profit/Losses" over the entire period
+# Calculate the changes in "Profit/Losses" over the entire period, then find the average of those changes
+# The greatest increase in profits (date and amount) over the entire period
+# The greatest decrease in profits (date and amount) over the entire period
+#
+# In addition, your final script should both print the analysis to the terminal and export a text file with the results
+
+print("Financial Analysis")
+print("------------------------------")
+print(f"Average Change: {avgChange}")
+print(f"Greatest Increase in profits: {maxIncDate}")
+print(f"The greatest decrease in profits: {maxDecDate}")
